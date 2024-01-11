2b0a52fc9f933bb6fcffd11fa06156fe812af017
matteomedori <matteo.medori.97@gmail.com>
2024-01-11 16:43:06 +0100

    feat: al click sul disco vengono mostrati i dati

--- server.php
+++ server.php
@@ -6,6 +6,17 @@ $database = file_get_contents(__DIR__ . '/dischi.json');
 //decodifico json 
 $dischi = json_decode($database);
 
+//se ho ricevuto una richiesta in get con un parametro id
+if (isset($_GET['id'])) {
+
+    //per salvare su file
+    // file_put_contents('dischi.json', json_encode($dischi));
+
+    header('Content-Type: application/json');
+    echo json_encode($dischi[$_GET['id']]);
+    die;
+}
+
 //dico che il tipo di dato ritornato dalla pagine deve essere json e stampo $dischi dopo averlo codificato in json
 header('Content-Type: application/json');
 echo json_encode($dischi);
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ createApp({
   data() {
     return {
       dischi: [],
+      infoDisco: null,
+      activeId: null,
     };
   },
   methods: {
@@ -11,6 +13,20 @@ createApp({
       axios.get("server.php").then((response) => {
         this.dischi = response.data;
       });
+    },
+    getInfo(id) {
+      if (this.infoDisco !== null && this.activeId === id) {
+        this.infoDisco = null;
+      } else {
+        this.activeId = id;
+        axios.get("server.php", { params: { id: id } }).then((response) => {
+          this.infoDisco = {
+            name: response.data.name,
+            author: response.data.author,
+            year: response.data.year,
+          };
+        });
+      }
     },
   },
   created() {
